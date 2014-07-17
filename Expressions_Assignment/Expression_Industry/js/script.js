e78178f07893ff90051e5bcdc1d54ae4ac960f22
digitalcreation <kvanburen@fullsail.edu>
2014-07-17 07:52:53 -0700

    Started setup for the $'s per hour sold calculation.

---
+++ Expressions_Assignment/Expression_Industry/js/script.js
@@ -0,0 +1,7 @@
+//Kevin Van Buren
+//Expression Assignment - Personal
+//July, 16 2014
+
+//The goal of this calculation is to show how many dollars ($) per hour a person is selling.
+
+var computerSold = prompt ("How many computer have you sold today?");
