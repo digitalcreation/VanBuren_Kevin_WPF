19acdb3a0c6eac6e0725924daefcbfe22aa4de1b
digitalcreation <kvanburen@fullsail.edu>
2014-07-16 20:44:36 -0700

    Setup dog food calculation. Set prompts for 1)Size of bag, 2)Cups fed a day, 3)Days desired

---
+++ Expressions_Assignment/Expression_Personal/js/script.js
@@ -0,0 +1,7 @@
+//Kevin Van Buren
+//Expression Assignment - Personal
+//July, 16 2014
+
+var bagSize = prompt ("How many pounds are in your dog food bag?"); //
+var cupDaily = prompt ("How many cups a day do you feed your dog?"); //
+var daysFood = prompt ("How many days do you want your food to last?"); //
