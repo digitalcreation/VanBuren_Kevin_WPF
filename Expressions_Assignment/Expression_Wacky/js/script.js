12d2b54af8964870fcf65730258d36304d25eb64
digitalcreation <kvanburen@fullsail.edu>
2014-07-17 15:37:24 -0700

    Setup Wacky Calculation. The goal of this calculation will be to find out how many potential ghosts are haunting a building. I setup an example prompt and alert of how the rest of the calculation might work.

---
+++ Expressions_Assignment/Expression_Wacky/js/script.js
@@ -0,0 +1,12 @@
+//Kevin Van Buren
+//Expression Assignment - Wacky
+//July, 16 2014
+
+//The goal of this calculation will be to find out how many potential ghosts are haunting a building.
+//The following will be an example of how some calculations might work.
+var ghostsHaunted = prompt ("To determine how many ghosts are haunting a building we first need to know: /nHow many people have died in or near the building?");
+if (ghostsHaunted > 4) {
+    alert("Yes!")
+}   else {
+    alert("No.")
+}
