9ee054e0dcbc3a101d8949cefa7c2601672027b0
digitalcreation <kvanburen@fullsail.edu>
2014-07-17 08:14:14 -0700

    Added prompt for total $ of ESP sold. Added console.log for all prompts. Added totalSold variable to figure out much in tech and esp is being sold.

--- Expressions_Assignment/Expression_Industry/js/script.js
+++ Expressions_Assignment/Expression_Industry/js/script.js
@@ -5,6 +5,16 @@
 //The goal of this calculation is to show how many dollars ($) per hour of Service Plans and Tech a person is selling.
 //Possibly to figure out the attach rate.
 
-var computerSold = prompt ("How many computer have you sold or brought in for repair today?");
-var ts1Sold = prompt ("How many computers have 1 year Total Support attached?");
-var ts2Sold = prompt ("How many computer have 2 year Total Support attached?");
+var computerSold = prompt ("How many computer have you sold or brought in for repair today?"); //
+console.log(computerSold); //
+var ts1Sold = prompt ("How many computers have 1 year Total Support attached?"); //
+console.log(ts1Sold); //
+var ts2Sold = prompt ("How many computer have 2 year Total Support attached?"); //
+console.log(ts2Sold); //
+var espSold = prompt ("How many dollars of ESP have you sold?"); //
+console.log(espSold); //
+
+var totalSold = ts1Soldold + ts2Sold + espSold; //
+console.log(totalSold); //
+
+//console.log();
