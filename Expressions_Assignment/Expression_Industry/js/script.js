ecbaf660202710eb18e54e0b3ae161e456fb0b0f
digitalcreation <kvanburen@fullsail.edu>
2014-07-17 07:58:26 -0700

    Setup prompt for computers sold or brought in for repair, how many 1 year total supports sold, and how many 2 year total support sold.

--- Expressions_Assignment/Expression_Industry/js/script.js
+++ Expressions_Assignment/Expression_Industry/js/script.js
@@ -1,7 +1,10 @@
 //Kevin Van Buren
-//Expression Assignment - Personal
+//Expression Assignment - Industry
 //July, 16 2014
 
-//The goal of this calculation is to show how many dollars ($) per hour a person is selling.
+//The goal of this calculation is to show how many dollars ($) per hour of Service Plans and Tech a person is selling.
+//Possibly to figure out the attach rate.
 
-var computerSold = prompt ("How many computer have you sold today?");
+var computerSold = prompt ("How many computer have you sold or brought in for repair today?");
+var ts1Sold = prompt ("How many computers have 1 year Total Support attached?");
+var ts2Sold = prompt ("How many computer have 2 year Total Support attached?");
