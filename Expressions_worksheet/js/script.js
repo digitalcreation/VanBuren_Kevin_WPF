afb184265a30f58740547292125d710d1181350a
digitalcreation <kvanburen@fullsail.edu>
2014-07-16 19:22:06 -0700

    Added Pizza problem part 1

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -7,5 +7,21 @@
 
 var sparkysAge = prompt("Enter Sparky's age:");
 var dogYears = sparkysAge * 7;
-var result = "Sparky is " + sparkysAge + " human years old which is " + dogYears + " in dog years.";
-alert(result);
+var sparkyResult = "Sparky is " + sparkysAge + " human years old which is " + dogYears + " in dog years.";
+alert(sparkyResult);
+//End sparky age calculation.
+
+//Number of slices each person ate at a party
+var pizzaSlices = 8;
+//per pizza - given
+var participants = 10;
+//number of party goers - given
+var wholePizzas = 3;
+//given
+var pizzaResult = (wholePizzas) * (pizzaSlices) / (participants);
+console.log (pizzaResult);
+var answer = "Each person ate " + (pizzaResult) + " slices of pizza at the party.";
+alert(answer);
+//End Pizza problem part 1
+
+//Start part 2
