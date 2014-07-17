e93983c1dcd7712e84c2703f4d513f5cb5f95dd6
digitalcreation <kvanburen@fullsail.edu>
2014-07-16 17:23:23 -0700

    Created calculation for dog (Sparky's) age. Added prompt for age, then alert for the result.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -5,3 +5,9 @@
 //Calculating a dogs age.
 //Dogs age 7x faster than human. Example: 1 human year x7 = 7 dog years.
 
+var sparkysAge = prompt("Enter Sparky's age:");
+var dogYears = sparkysAge * 7;
+var result = "Sparky is " + sparkysAge + " years old which is " + dogYears + " in dog years.";
+alert(result);
+
+console.log;
