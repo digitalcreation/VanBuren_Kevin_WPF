97f84aea85bf75172179c9e7c6843a749ade077a
digitalcreation <kvanburen@fullsail.edu>
2014-07-17 19:04:47 -0700

    Added console.log for all prompts with helpful notes for each. Commented the typeDeath result out as it is not ready yet.

--- Expressions_Assignment/Expression_Wacky/js/script.js
+++ Expressions_Assignment/Expression_Wacky/js/script.js
@@ -4,13 +4,17 @@
 
 //The goal of this calculation will be to find out how many potential ghosts are haunting a building.
 alert("To determine how many ghosts are haunting a building you need to know... (Click Ok to continue)");
-var ghostsHaunted = prompt ("How many people have died in or near the building?");
+var scary = prompt ("How many times have you seen something unexplainable happen? This can range from doors shutting suddenly to hearing odd noises.");
+console.log(scary + " :Prompts user for how times something odd or unexplainable happened.");
 
-var scary = prompt ("How many times have you seen something unexplainable happen?");
+var ghostsHaunted = prompt ("How many people have died in or near the building?");
+console.log(ghostsHaunted + " :Prompts user for how many deaths happened in or in the building.");
 
 var typeDeath = prompt ("On a scale of 1-10 how gruesome was the average death?");
-if (typeDeath < 4) {
-    alert("The building isn't very haunted")
-}   else {
-    alert("The building is really haunted! If you are in it, get out now!")
-}
+console.log(typeDeath + " :Prompts user for how gruesome the average death was - on a scale of 1-10");
+
+//if (typeDeath < 4) {
+  //  alert("The building isn't very haunted")
+//}   else {
+    //alert("The building is really haunted! If you are in it, get out now!")
+//}
