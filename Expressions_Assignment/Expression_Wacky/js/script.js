16c3b3c119ebdfb88021f24b691ac41e247d636c
digitalcreation <kvanburen@fullsail.edu>
2014-07-17 19:11:03 -0700

    added totalScary variable to calculate just how haunted or scary the building really is. Added console.log with meaningful comments.

--- Expressions_Assignment/Expression_Wacky/js/script.js
+++ Expressions_Assignment/Expression_Wacky/js/script.js
@@ -3,7 +3,9 @@
 //July, 16 2014
 
 //The goal of this calculation will be to find out how many potential ghosts are haunting a building.
-alert("To determine how many ghosts are haunting a building you need to know... (Click Ok to continue)");
+
+alert("To determine how many ghosts are haunting a building you need to know... (Click 'Ok' to continue)");
+
 var scary = prompt ("How many times have you seen something unexplainable happen? This can range from doors shutting suddenly to hearing odd noises.");
 console.log(scary + " :Prompts user for how times something odd or unexplainable happened.");
 
@@ -12,6 +14,9 @@ console.log(ghostsHaunted + " :Prompts user for how many deaths happened in or i
 
 var typeDeath = prompt ("On a scale of 1-10 how gruesome was the average death?");
 console.log(typeDeath + " :Prompts user for how gruesome the average death was - on a scale of 1-10");
+
+var totalScary = parseFloat(scary) + parseFloat(ghostsHaunted) + parseFloat(typeDeath);
+console.log(totalScary + " :Shows the total number from the prompts.");
 
 //if (typeDeath < 4) {
   //  alert("The building isn't very haunted")
