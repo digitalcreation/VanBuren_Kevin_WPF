63b9c97b473b05b52efa827a9aa0be73f43a2039
digitalcreation <kvanburen@fullsail.edu>
2014-07-17 18:55:57 -0700

    Added an intro alert message. Added variable for how gruesome the average death was, and how many times something unexplainable has happened.

--- Expressions_Assignment/Expression_Wacky/js/script.js
+++ Expressions_Assignment/Expression_Wacky/js/script.js
@@ -3,10 +3,14 @@
 //July, 16 2014
 
 //The goal of this calculation will be to find out how many potential ghosts are haunting a building.
-//The following will be an example of how some calculations might work.
-var ghostsHaunted = prompt ("To determine how many ghosts are haunting a building we first need to know: /nHow many people have died in or near the building?");
-if (ghostsHaunted > 4) {
-    alert("Yes!")
+alert("To determine how many ghosts are haunting a building you need to know... (Click Ok to continue)");
+var ghostsHaunted = prompt ("How many people have died in or near the building?");
+
+var scary = prompt ("How many times have you seen something unexplainable happen?");
+
+var typeDeath = prompt ("On a scale of 1-10 how gruesome was the average death?");
+if (typeDeath < 4) {
+    alert("The building isn't very haunted")
 }   else {
-    alert("No.")
+    alert("The building is really haunted! If you are in it, get out now!")
 }
