29abd3b552cc99d7a69fbf3a81d68556e75428aa
digitalcreation <kvanburen@fullsail.edu>
2014-07-24 09:00:21 -0700

    Added if that will not allow the user to leave the field blank, if so it will reprompt.

--- Conditionals_Assignment/Conditionals_Personal/js/script.js
+++ Conditionals_Assignment/Conditionals_Personal/js/script.js
@@ -5,4 +5,8 @@
 //Goal is to setup a calculation for counting the calories eaten in the day and to alert if it is above or below the recommend number.
 
 var caloriesEaten = prompt("Enter in the total number of calories you have eaten today.");//This will prompt the user to enter in the total number of calories eaten.
-console.log(caloriesEaten + " - The number of calories entered in by the user."); //This will record the number of calories entered by the user.
+if (caloriesEaten === ""){ //Not allowing the user to leave the text field blank
+    alert("You CANNOT leave the text field blank, enter in a number."); //Alerting the user that they can't leave the text field empty.
+    console.log("Alert was sent because text field was left blank."); //Added to record the alert
+    caloriesEaten = prompt("Enter in the total number of calories you have eaten today.");//This will prompt the user to enter in the total number of calories eaten.
+} //end of if
