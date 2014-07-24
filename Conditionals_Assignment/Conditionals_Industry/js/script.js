3825ed5515de0e384e9f63a849e16f6c9eae61c4
digitalcreation <kvanburen@fullsail.edu>
2014-07-23 23:09:54 -0700

    Started to add comments for all lines. added console.log for store sales empty text field. added re-prompt for store sales if first field was left empty. commented out if for min store sales. added ternary for min store sales. changed min sales to <= 30. added alert for penrate.

--- Conditionals_Assignment/Conditionals_Industry/js/script.js
+++ Conditionals_Assignment/Conditionals_Industry/js/script.js
@@ -5,21 +5,22 @@
 //Setup a calculation for calculating overall EasyTech sales penetration and
 //factors needed: 1) Total week-to-date store sales. 2) Total week-to-date EasyTech sales. 3) the day of the week.
 //
-var dayOfWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+//var dayOfWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
-var storeSales = prompt ("Enter your week-to-date store sales, e.g. 20000:");
-console.log  (storeSales, " - User prompt");
-if (storeSales === ""){
-    alert("You CANNOT leave the text field blank, enter in a number.");
-    console.log("Alert if field is left blank.");
-    storeSales = prompt ("Enter your week-to-date store sales, e.g. 20000:");
-}
-if (storeSales >= 0){
+var storeSales = prompt ("Enter your week-to-date store sales, e.g. $20000:"); //This will prompt the user for the first given: the Store week to date sales
+console.log  (storeSales, " - User prompt"); //Used to log the users input
+if (storeSales === ""){ //Not allowing the user to leave the text field blank
+    alert("You CANNOT leave the text field blank, enter in a number."); //Alerting the user that they can't leave the text field empty.
+    console.log("Alert was sent because text field was left blank."); //Added to record the alert
+    storeSales = prompt ("Enter your week-to-date store sales, e.g. $20000:"); //Prompting the user again to enter in the stores total sales.
+} //end of if
+(storeSales >= 30) ? console.log("Store sales are over the min of #30.") : storeSales = prompt ("Your store sales CANNOT be less than $30 - the lowest cost EasyTech service. Enter your week-to-date store sales, e.g. $20000:"); //Prompting the user again to enter in the stores total sales.;
+/*if (storeSales >= 1){ //
     console.log( "Confirming user entered a number")
-}
+}*/
 
-var etSales = prompt ("Enter your week-to-date EasyTech sales, e.g. 1200:");
+var etSales = prompt ("Enter your week-to-date EasyTech sales, e.g. $1200:");
 
 
-var penRate =  (etSales / storeSales);
+var penRate =  alert (etSales / storeSales);
 console.log(penRate + " Penetration rate.");
