f849f74ecd11817844b32d72870cf0c7956a30e7
digitalcreation <kvanburen@fullsail.edu>
2014-07-23 23:32:10 -0700

    removed commented out lines that arent of use anymore. fixed console.log for store sales to show the number entered in by the user. removed the " - lowest cost tech service." line in the reprompt for store sales.

--- Conditionals_Assignment/Conditionals_Industry/js/script.js
+++ Conditionals_Assignment/Conditionals_Industry/js/script.js
@@ -3,21 +3,17 @@
 //07-23-14
 
 //Setup a calculation for calculating overall EasyTech sales penetration and
-//factors needed: 1) Total week-to-date store sales. 2) Total week-to-date EasyTech sales. 3) the day of the week.
-//
-//var dayOfWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+//factors needed: 1) Total week-to-date store sales. 2) Total week-to-date EasyTech sales.
 
 var storeSales = prompt ("Enter your week-to-date store sales, e.g. $20000:"); //This will prompt the user for the first given: the Store week to date sales
-console.log  (storeSales, " - User prompt"); //Used to log the users input
+console.log  (storeSales + " - Store sales entered by user."); //Used to log the users input
 if (storeSales === ""){ //Not allowing the user to leave the text field blank
     alert("You CANNOT leave the text field blank, enter in a number."); //Alerting the user that they can't leave the text field empty.
     console.log("Alert was sent because text field was left blank."); //Added to record the alert
     storeSales = prompt ("Enter your week-to-date store sales, e.g. $20000:"); //Prompting the user again to enter in the stores total sales.
 } //end of if
-(storeSales >= 30) ? console.log("Store sales are over the min of #30.") : storeSales = prompt ("Your store sales CANNOT be less than $30 - the lowest cost EasyTech service. Enter your week-to-date store sales, e.g. $20000:"); //Prompting the user again to enter in the stores total sales.;
-/*if (storeSales >= 1){ //
-    console.log( "Confirming user entered a number")
-}*/
+(storeSales >= 30) ? console.log("Store sales are over the min of $30.") : storeSales = prompt ("Your store sales CANNOT be less than $30. Enter your week-to-date store sales, e.g. $20000:"); //Prompting the user again to enter in the stores total sales.;
+
 
 var etSales = prompt ("Enter your week-to-date EasyTech sales, e.g. $1200:");
 console.log(etSales + " - EasyTech sales entered by user.");
