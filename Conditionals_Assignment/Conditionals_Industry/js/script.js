4bcb20855675d9b80cc9ce3579ad3fbed999a5d9
digitalcreation <kvanburen@fullsail.edu>
2014-07-24 08:08:04 -0700

    Duplicated if and ternary for storesales for a better loop. Added if and ternary for etSales, set min etSales to 30.

--- Conditionals_Assignment/Conditionals_Industry/js/script.js
+++ Conditionals_Assignment/Conditionals_Industry/js/script.js
@@ -13,10 +13,22 @@ if (storeSales === ""){ //Not allowing the user to leave the text field blank
     storeSales = prompt ("Enter your week-to-date store sales, e.g. $20000:"); //Prompting the user again to enter in the stores total sales.
 } //end of if
 (storeSales >= 30) ? console.log("Store sales are over the min of $30.") : storeSales = prompt ("Your store sales CANNOT be less than $30. Enter your week-to-date store sales, e.g. $20000:"); //Prompting the user again to enter in the stores total sales.;
+if (storeSales === ""){ //Not allowing the user to leave the text field blank
+    alert("You CANNOT leave the text field blank, enter in a number."); //Alerting the user that they can't leave the text field empty.
+    console.log("Alert was sent because text field was left blank."); //Added to record the alert
+    storeSales = prompt ("Enter your week-to-date store sales, e.g. $20000:"); //Prompting the user again to enter in the stores total sales.
+} //end of if
+(storeSales >= 30) ? console.log("Store sales are over the min of $30.") : storeSales = prompt ("Your store sales CANNOT be less than $30. Enter your week-to-date store sales, e.g. $20000:"); //Prompting the user again to enter in the stores total sales.;
 
 
 var etSales = prompt ("Enter your week-to-date EasyTech sales, e.g. $1200:");
 console.log(etSales + " - EasyTech sales entered by user.");
+if (etSales === ""){
+    alert("You CANNOT leave the text field blank, enter in a number."); //Alerting the user that they can't leave the text field empty.
+    console.log("Alert was sent because text field was left blank."); //Added to record the alert
+    etSales = prompt ("Enter your week-to-date EasyTech sales, e.g. $1200:");
+}
+(etSales >= 30) ? console.log("EasyTech sales are over the min of $30.") : etSales = prompt ("Your EasyTech sales CANNOT be less than $30. Enter your week-to-date EasyTech sales, e.g. $1200:"); //Prompting the user again to enter in the stores total sales.;
 
 
 var penRate =  alert ("Your EasyTech penetration is " + etSales / storeSales);
