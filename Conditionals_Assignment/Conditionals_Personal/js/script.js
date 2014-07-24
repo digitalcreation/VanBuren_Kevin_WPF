d761f89c3b4aa58bf75d818fa3f4be4d986acca4
digitalcreation <kvanburen@fullsail.edu>
2014-07-24 15:20:55 -0700

    Fixed snacks calorie if

--- Conditionals_Assignment/Conditionals_Personal/js/script.js
+++ Conditionals_Assignment/Conditionals_Personal/js/script.js
@@ -14,6 +14,7 @@ if (caloriesBreakfast === ""){ //Not allowing the user to leave the text field b
 } //end of if
 console.log(caloriesBreakfast + " - Calories for breakfast, entered by user."); //Used to record cals eaten for breakfast.
 
+
 var caloriesLunch = prompt("Enter in the total number of calories you had for lunch.");//This will prompt the user to enter in the total number of calories eaten.
 if (caloriesLunch === ""){
     alert("You CANNOT leave the text field blank, enter in a number."); //Alerting the user that they can't leave the text field empty.
@@ -21,6 +22,7 @@ if (caloriesLunch === ""){
     caloriesLunch = prompt("Enter in the total number of calories you had for lunch, e.g. 500.");//This will prompt the user to enter in the total number of calories eaten.
 } //end of if
 console.log(caloriesLunch + " - Calories for lunch, entered by user."); //Used to record cals eaten for lunch.
+
 
 var caloriesDinner = prompt("Enter in the total number of calories you had for dinner.");//This will prompt the user to enter in the total number of calories eaten.
 if (caloriesDinner === ""){
@@ -30,16 +32,19 @@ if (caloriesDinner === ""){
 } //end of if
 console.log(caloriesDinner + " - Calories for dinner, entered by user.");//Used to record cals eaten for dinner.
 
+
 var caloriesSnack = prompt("Enter in the total number of calories you had for any snacks throughout the day.");//This will prompt the user to enter in the total number of calories eaten.
-if (caloriesDinner === ""){
+if (caloriesSnack === ""){
     alert("You CANNOT leave the text field blank, enter in a number."); //Alerting the user that they can't leave the text field empty.
     console.log("Alert was sent because snack text field was left blank."); //Added to record the alert
-    caloriesDinner = prompt("Enter in the total number of calories you had for any snacks throughout the day, e.g. 500.");//This will prompt the user to enter in the total number of calories eaten.
+    caloriesSnack = prompt("Enter in the total number of calories you had for any snacks throughout the day, e.g. 500.");//This will prompt the user to enter in the total number of calories eaten.
 } //end of if
 console.log(caloriesSnack + " - Calories for snacks, entered by user.");//Used to record cals eaten for snacks.
 
+
 var totalCalories = Number(caloriesBreakfast) + Number(caloriesDinner) + Number(caloriesLunch) + Number(caloriesSnack);
 console.log(totalCalories + " - Total calories calculation");//Used to record total calories eaten.
+
 
 
 var userWeight = prompt("To determine if you have eaten too many or not enough calories, enter in your weight.");
