523c684896b63830dd7de88bebaac397de20978f
digitalcreation <kvanburen@fullsail.edu>
2014-07-24 08:45:52 -0700

    added comment for the overall goal of the calculation. added variable that will prompt the user to enter in the total number of calories. added console.log to record this number.

--- Conditionals_Assignment/Conditionals_Personal/js/script.js
+++ Conditionals_Assignment/Conditionals_Personal/js/script.js
@@ -2,4 +2,7 @@
 //Conditionals Assignment - Personal
 //07-24-14
 
-alert("This is in the js file");
+//Goal is to setup a calculation for counting the calories eaten in the day and to alert if it is above or below the recommend number.
+
+var caloriesEaten = prompt("Enter in the total number of calories you have eaten today.");//This will prompt the user to enter in the total number of calories eaten.
+console.log(caloriesEaten + " - The number of calories entered in by the user."); //This will record the number of calories entered by the user.
