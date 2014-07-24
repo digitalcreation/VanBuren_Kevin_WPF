4273dceaafec646d933a8b9c6847078d15bfd159
digitalcreation <kvanburen@fullsail.edu>
2014-07-24 08:30:33 -0700

    Setup ternary for penRate, which will alert the user of their current penetration rate and let them know if they on or bleow goal. The min for on goal is 4%. Added several comments to show what is being done on each line.

--- Conditionals_Assignment/Conditionals_Industry/js/script.js
+++ Conditionals_Assignment/Conditionals_Industry/js/script.js
@@ -12,7 +12,7 @@ if (storeSales === ""){ //Not allowing the user to leave the text field blank
     console.log("Alert was sent because text field was left blank."); //Added to record the alert
     storeSales = prompt ("Enter your week-to-date store sales, e.g. $30000:"); //Prompting the user again to enter in the stores total sales.
 } //end of if
-(storeSales >= 30) ? console.log("Store sales are over the min of $30.") : storeSales = prompt ("Your store sales CANNOT be less than $30. Enter your week-to-date store sales, e.g. $20000:"); //If store sales are over 30miin, record in console. Otherwise, prompting the user again to enter in the stores total sales.;
+(storeSales >= 30) ? console.log("Store sales are over the min of $30.") : storeSales = prompt ("Your store sales CANNOT be less than $30. Enter your week-to-date store sales, e.g. $20000:"); //If store sales are over $30min, record in console. Otherwise, prompting the user again to enter in the stores total sales.;
 if (storeSales === ""){ //Not allowing the user to leave the text field blank
     alert("You CANNOT leave the text field blank, enter in a number."); //Alerting the user that they can't leave the text field empty.
     console.log("Alert was sent because text field was left blank."); //Added to record the alert
@@ -21,15 +21,16 @@ if (storeSales === ""){ //Not allowing the user to leave the text field blank
 (storeSales >= 30) ? console.log("Store sales are over the min of $30.") : storeSales = prompt ("Your store sales CANNOT be less than $30. Enter your week-to-date store sales, e.g. $20000:"); //Prompting the user again to enter in the stores total sales.;
 
 
-var etSales = prompt ("Enter your week-to-date EasyTech sales, e.g. $1200:");
-console.log(etSales + " - EasyTech sales entered by user.");
-if (etSales === ""){
+var etSales = prompt ("Enter your week-to-date EasyTech sales, e.g. $1200:"); //Prompting the user to WTD EasyTech sales.
+console.log(etSales + " - EasyTech sales entered by user."); //Recording the users WTD EasyTech sales.
+if (etSales === ""){ //Not allowing the user to leave the text field blank.
     alert("You CANNOT leave the text field blank, enter in a number."); //Alerting the user that they can't leave the text field empty.
     console.log("Alert was sent because text field was left blank."); //Added to record the alert
     etSales = prompt ("Enter your week-to-date EasyTech sales, e.g. $1200:"); //Prompting the user again to enter in the total EasyTech sales.
 } //end of if
-(etSales >= 30) ? console.log("EasyTech sales are over the min of $30.") : etSales = prompt ("Your EasyTech sales CANNOT be less than $30. Enter your week-to-date EasyTech sales, e.g. $1200:"); ///If EasyTech sales are over 30miin, record in console. Otherwise, prompting the user again to enter in the stores total sales.;
+(etSales >= 30) ? console.log("EasyTech sales are over the min of $30.") : etSales = prompt ("Your EasyTech sales CANNOT be less than $30. Enter your week-to-date EasyTech sales, e.g. $1200:"); //If EasyTech sales are over $30min, record in console. Otherwise, prompting the user again to enter in the EasyTech total sales.;
 
-var penRate =  (etSales / storeSales * 100);
-console.log(penRate + "% Penetration rate.");
-alert ("You have a current EasyTech penetration rate of " + penRate + "%.");
+
+var penRate =  (etSales / storeSales * 100); //Used to calculate the users penetration rate.
+console.log(penRate + "% Penetration rate."); //Records the users penetration rate.
+(penRate >= 4) ? alert ("You have a current EasyTech penetration rate of " + penRate + "%. You are on goal!") : alert ("You have a current EasyTech penetration rate of " + penRate + "%. You are below goal!"); //If pen rate is above 4$, on goal. Otherwise, below goal.;
