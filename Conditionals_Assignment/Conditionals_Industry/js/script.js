b00bafc2f6acd3a6e110bd688a9ec8b87ab8c7ec
digitalcreation <kvanburen@fullsail.edu>
2014-07-23 22:21:28 -0700

    added etSales variable and penRate variable which will give the final answer. Added = to storeSales so it is >= 0 added e.g. in prompts to help the user find the right information for entry

--- Conditionals_Assignment/Conditionals_Industry/js/script.js
+++ Conditionals_Assignment/Conditionals_Industry/js/script.js
@@ -3,17 +3,23 @@
 //07-23-14
 
 //Setup a calculation for calculating overall EasyTech sales penetration and
-//factors needed: 1) Total week-to-date store sales. 2) Total week-to-date EasyTech sales.
+//factors needed: 1) Total week-to-date store sales. 2) Total week-to-date EasyTech sales. 3) the day of the week.
 //
 var dayOfWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-var storeSales = prompt ("Enter your week-to-date store sales:");
+
+var storeSales = prompt ("Enter your week-to-date store sales, e.g. 20000:");
 console.log  (storeSales, " - User prompt");
 if (storeSales === ""){
     alert("You CANNOT leave the text field blank, enter in a number.");
-    console.log(" Alert if field is left blank.");
-    storeSales = prompt ("Enter your week-to-date store sales:");
-
+    console.log("Alert if field is left blank.");
+    storeSales = prompt ("Enter your week-to-date store sales, e.g. 20000:");
 }
-if (storeSales > 0){
+if (storeSales >= 0){
     console.log( "Confirming user entered a number")
 }
+
+var etSales = prompt ("Enter your week-to-date EasyTech sales, e.g. 1200:");
+
+
+var penRate =  (etSales / storeSales);
+console.log(penRate + " Penetration rate.");
