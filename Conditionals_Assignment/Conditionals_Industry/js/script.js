e4b2f74368ba8f60d62a7f2c229b05384cca38cd
digitalcreation <kvanburen@fullsail.edu>
2014-07-23 21:45:28 -0700

    added variable for dayOfWeek and storeSales setup if for if the user leave the text field blank and if the user entered in a number greater than 0.

--- Conditionals_Assignment/Conditionals_Industry/js/script.js
+++ Conditionals_Assignment/Conditionals_Industry/js/script.js
@@ -2,4 +2,18 @@
 //Conditionals Assignment - Industry
 //07-23-14
 
-alert ("Testing");
+//Setup a calculation for calculating overall EasyTech sales penetration and
+//factors needed: 1) Total week-to-date store sales. 2) Total week-to-date EasyTech sales.
+//
+var dayOfWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var storeSales = prompt ("Enter your week-to-date store sales:");
+console.log  (storeSales, " - User prompt");
+if (storeSales === ""){
+    alert("You CANNOT leave the text field blank, enter in a number.");
+    console.log(" Alert if field is left blank.");
+    storeSales = prompt ("Enter your week-to-date store sales:");
+
+}
+if (storeSales > 0){
+    console.log( "Confirming user entered a number")
+}
