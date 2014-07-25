f762e06d306eb42ca7a0e6393494eda99efcf222
digitalcreation <kvanburen@fullsail.edu>
2014-07-24 18:46:25 -0700

    Added comments on what will need to be added. added initial alert which shows the user what the site will be calculating. added prompt for first monkey stat.

--- Conditionals_Assignment/Conditionals_Wacky/js/script.js
+++ Conditionals_Assignment/Conditionals_Wacky/js/script.js
@@ -2,4 +2,16 @@
 //Conditional Assignment - Wacky
 //07-24-14
 
-alert("This is in the js file");
+
+//How many monkeys does it take to screw in a light bulb
+//Factors needed for their intelligence -
+// 1) How often do the monkeys groom each other? (increase in intel)
+// 2) How often do the monkeys fling their feces? (decrease in intel)
+// 3) How many hours a day do the monkeys play games with each other? (increase in intel)
+// 4) On a scale of 1 to 10, how aggressive are they? (decrease in intel)
+
+alert("How many monkey's does it take to screw in a light bulb? To find the solution we need to know 4 things..."); //Alerting the user of what the calculation will be.
+
+var monkeyGroom = prompt("How often do the monkeys groom each other a day?"); //Prompts the user to enter in a number amount.
+console.log(monkeyGroom + " - How often monkeys groom each other, entered by user"); //Records the number entered in by the user.
+
