aaf441d3c2f8b4f576c75d3f4f0b1e66ecb3f329
digitalcreation <kvanburen@fullsail.edu>
2014-07-24 19:15:26 -0700

    Updated comments. added prompt variable for monkey aggression and console.log to record entry.

--- Conditionals_Assignment/Conditionals_Wacky/js/script.js
+++ Conditionals_Assignment/Conditionals_Wacky/js/script.js
@@ -12,13 +12,16 @@
 
 alert("How many monkey's does it take to screw in a light bulb? To find the solution we need to know 4 things..."); //Alerting the user of what the calculation will be.
 
-var monkeyGroom = prompt("How often do the monkey's groom each other a day?"); //Prompts the user to enter in a number amount.
-console.log(monkeyGroom + " - How often monkey's groom each other, entered by user"); //Records the number entered in by the user.
+var monkeyGroom = prompt("How often do the monkey's groom each other a day?"); //Prompts the user to enter in a number amount for how many times a day the monkey's groom each other.
+console.log(monkeyGroom + " - How often monkey's groom each other, entered by user."); //Records the number entered in by the user.
 
-var monkeyFling = prompt("How many times a day do the monkey's fling their feces?"); //Prompts the user to enter in a number amount.
-console.log(monkeyFling + " - How often monkey's fling feces at each other, entered by user"); //Records the number entered in by the user.
+var monkeyFling = prompt("How many times a day do the monkey's fling their feces?"); //Prompts the user to enter in a number amount for how many times a day the monkey's fling their feces.
+console.log(monkeyFling + " - How often monkey's fling feces at each other, entered by user."); //Records the number entered in by the user.
 
-var monkeyPlay = prompt("How many hours a day do the monkey's play games with each other?");
-console.log(monkeyPlay + " - How many hours/day do the monkey's play games, entered by user");
+var monkeyPlay = prompt("How many hours a day do the monkey's play games with each other?"); //Prompts the user to enter in a number amount for how long the monkey's play a day.
+console.log(monkeyPlay + " - How many hours/day the monkey's play games, entered by user."); //Records the number entered in by the user.
+
+var monkeyAggression = prompt("On a scale of 1 to 10, how aggressive are the monkey's? - 10 being really aggressive."); //Prompts the user to enter in a number amount of how aggressive the monkey's are.
+console.log(monkeyAggression + " - Scale from 1-10 on how aggressive the monkey's are, entered by user."); //Records the number entered in by the user.
 
 
