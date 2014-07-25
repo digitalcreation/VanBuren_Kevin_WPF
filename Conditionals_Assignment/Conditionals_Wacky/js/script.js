9ed47c8e4dc39372b85b45ee6e972e9d83d6a3de
digitalcreation <kvanburen@fullsail.edu>
2014-07-24 19:42:31 -0700

    Added if for monkeyFling variable; if user leave field blank, alerts then reprompts and if the number is over 20 for monkeyFling then it will show an alert notifying the user of this.

--- Conditionals_Assignment/Conditionals_Wacky/js/script.js
+++ Conditionals_Assignment/Conditionals_Wacky/js/script.js
@@ -16,14 +16,21 @@ var monkeyGroom = prompt("How often do the monkey's groom each other a day?"); /
 console.log(monkeyGroom + " - How often monkey's groom each other, entered by user."); //Records the number entered in by the user.
 if (monkeyGroom === ""){
     alert("For this scientific calculation to be accurate you need to enter in a numerical value!"); //Alerts the user they need to enter in a number.
-    monkeyGroom = prompt("How often do the monkey's groom each other a day? E.g. 12."); //Re-prompts the user to enter in a number amount for how many times a day the monkey's groom each other.
+    monkeyGroom = prompt("How often do the monkey's groom each other a day? e.g. 12."); //Re-prompts the user to enter in a number amount for how many times a day the monkey's groom each other.
 }
-if (monkeyGroom >= 30){ //Setting up for if the user enters in a large number.
+if (monkeyGroom >= 30){ //Setting up for an alert if the user enters in a large number.
     alert("Wow! Maybe the monkey's should open up a lice removal shop!"); //Alerts the user that over 30 is large number for the equation
 } //end of if
 
 var monkeyFling = prompt("How many times a day do the monkey's fling their feces?"); //Prompts the user to enter in a number amount for how many times a day the monkey's fling their feces.
 console.log(monkeyFling + " - How often monkey's fling feces at each other, entered by user."); //Records the number entered in by the user.
+if (monkeyFling === ""){
+    alert("For this scientific calculation to be accurate you need to enter in a numerical value!"); //Alerts the user they need to enter in a number.
+    monkeyFling = prompt("How many times a day do the monkey's fling their feces? e.g. 10."); //Prompts the user to enter in a number amount for how many times a day the monkey's fling their feces.
+}
+if (monkeyFling >= 20){ //Setting up for an alert if the user enters in a large number.
+    alert("Wow! Be sure to keep an eye out for flying fecal matter!"); //Alerts the user that over 30 is large number for the equation
+} //end of if
 
 var monkeyPlay = prompt("How many hours a day do the monkey's play games with each other?"); //Prompts the user to enter in a number amount for how long the monkey's play a day.
 console.log(monkeyPlay + " - How many hours/day the monkey's play games, entered by user."); //Records the number entered in by the user.
@@ -33,4 +40,4 @@ console.log(monkeyAggression + " - Scale from 1-10 on how aggressive the monkey'
 
 var monkeyIntelligence = Number(monkeyGroom) - Number(monkeyFling) + Number(monkeyPlay) - Number(monkeyAggression); //The scientific calculation to determine how many monkey's it takes to screw in a light bulb.
 console.log(monkeyIntelligence + " - The number of monkeys it takes to screw in a light bulb - the final result."); //Records how many monkey's it takes to screw in a light bulb.
-alert("It takes " + monkeyIntelligence + " monkey's to screw in a light bulb."); //Alerts the user of how many monkey's it takes to screw in a light bulb.
+alert("It takes " + monkeyIntelligence + " monkey(s) to screw in a light bulb."); //Alerts the user of how many monkey's it takes to screw in a light bulb.
