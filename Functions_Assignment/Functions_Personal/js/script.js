f657d97a37cf2b67c0f3f260ee640f86a48e0c06
digitalcreation <kvanburen@fullsail.edu>
2014-07-30 23:45:54 -0700

    Setup variable for hours slept & for loop for loop: if left blank, reprompt. if not a # reprompt. - also moved console.log for workHours below loop.

--- Functions_Assignment/Functions_Personal/js/script.js
+++ Functions_Assignment/Functions_Personal/js/script.js
@@ -5,11 +5,16 @@
 //This calculation will calculate your overall productivity with a percent value.
 
 var workHours= prompt("How many hours a day do you work?");
-console.log(workHours + " - Number of hours worked, entered by the user.");
 for(workHours ; isNaN(workHours) || workHours === "";){
     workHours= prompt("How many hours a day do you work? You cannot leave the text field blank and you must enter in a number.");
 }
+console.log(workHours + " - Number of hours worked, entered by the user.");
 
+var sleepHours = prompt ("How many hours a day do you sleep?");
+for(sleepHours ; isNaN(sleepHours) || sleepHours === "";){
+    sleepHours= prompt("How many hours a day do you sleep? You cannot leave the text field blank and you must enter in a number.");
+}
+console.log(sleepHours + " - Number of hours slept, entered by the user.");
 //The following comment is used to easily setup NaN and "" while loop.
 /*
  while(isNaN() ||  === ""){
