d94deae9263f61ac8661500913dd5eb8f3d92434
digitalcreation <kvanburen@fullsail.edu>
2014-07-30 23:35:44 -0700

    Setup variable for hours worked and for loop variable prompts user for how many hours they worked and for loop checks to see if the user left the text field blank or didn't enter a number.

--- Functions_Assignment/Functions_Personal/js/script.js
+++ Functions_Assignment/Functions_Personal/js/script.js
@@ -2,4 +2,17 @@
 //Functions Assignment - Personal
 //July, 30 2014
 
-alert("This is in the js file");
+//This calculation will calculate your overall productivity with a percent value.
+
+var workHours= prompt("How many hours a day do you work?");
+console.log(workHours + " - Number of hours worked, entered by the user.");
+for(workHours ; isNaN(workHours) || workHours === "";){
+    workHours= prompt("How many hours a day do you work? You cannot leave the text field blank and you must enter in a number.");
+}
+
+//The following comment is used to easily setup NaN and "" while loop.
+/*
+ while(isNaN() ||  === ""){
+
+ }
+ */
