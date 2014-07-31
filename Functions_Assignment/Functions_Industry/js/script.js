7d8b1ca6b851fc965871500b8b87d12ecedf53aa
digitalcreation <kvanburen@fullsail.edu>
2014-07-30 17:21:44 -0700

    Added intro alert to make the user aware of what the calculation will be used for. Added variables for how many: computers were sold, how many had 1 year Total Support (TS), and how mnany had 2 year TS attached. Also added console.log to store all of the numbers

--- Functions_Assignment/Functions_Industry/js/script.js
+++ Functions_Assignment/Functions_Industry/js/script.js
@@ -2,4 +2,15 @@
 //Functions Assignment - Industry
 //July, 30 2014
 
-//alert("This is in the js file");
+//This calculation will to calculate the total market basket $'s and penetration.
+
+alert("This site will help calculate how many Market Basket (MB) dollars you've sold and your current MB penetration rate."); //Alerts the user of what to expect from this calculation
+
+var pcSold = prompt("How many computers have you sold?"); //Prompts the user for how many computers have been sold, will be used for penetration.
+console.log(pcSold + " - Number of computers sold, entered by the user."); //Stores the number of computers sold that the user entered.
+
+var ts1Sold = prompt("How many of the computers sold had 1 year Total Support attached?"); //Prompts the user for how many computers have 1 year total support attached, used to calculate market basket $'s and pen rate.
+console.log(ts1Sold + " - Number of 1 year Total Supports sold, entered by the user."); //Stores the number of 1 year total supports sold that the user entered.
+
+var ts2Sold = prompt("How many of the computers sold had 2 year Total Support attached?"); //Prompts the user for how many computers have 2 year total support attached, used to calculate market basket $'s and pen rate.
+console.log(ts2Sold + " - Number of 2 year Total Supports sold, entered by the user."); //Stores the number of 1 year total supports sold that the user entered.
