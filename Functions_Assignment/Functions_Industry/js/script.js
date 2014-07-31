2559c3957da4ba97d6273b737c7782abcc8f2982
digitalcreation <kvanburen@fullsail.edu>
2014-07-30 17:35:30 -0700

    Setup variable for other accessories sold and console.log to store the number.

--- Functions_Assignment/Functions_Industry/js/script.js
+++ Functions_Assignment/Functions_Industry/js/script.js
@@ -13,4 +13,7 @@ var ts1Sold = prompt("How many of the computers sold had 1 year Total Support at
 console.log(ts1Sold + " - Number of 1 year Total Supports sold, entered by the user."); //Stores the number of 1 year total supports sold that the user entered.
 
 var ts2Sold = prompt("How many of the computers sold had 2 year Total Support attached?"); //Prompts the user for how many computers have 2 year total support attached, used to calculate market basket $'s and pen rate.
-console.log(ts2Sold + " - Number of 2 year Total Supports sold, entered by the user."); //Stores the number of 1 year total supports sold that the user entered.
+console.log(ts2Sold + " - Number of 2 year Total Supports sold, entered by the user."); //Stores the number of 2 year total supports sold that the user entered.
+
+var accSold = prompt("Enter in the total dollars of other accessories (software, misc EasyTech services, bags/covers, etc) that were sold. Round to the nearest dollar and exclude the '$'.");
+console.log("$" + accSold + " - Total dollars($) of other accessories sold, entered by the user."); //Stores the $'s of other accessories sold that the user entered.
