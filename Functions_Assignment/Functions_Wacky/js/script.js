2c614e857a6b60c58613b52014238b9c7bb1dece
digitalcreation <kvanburen@fullsail.edu>
2014-07-31 10:29:13 -0700

    Added loop for user age, function to calculate how long user will live as a Time Lord. Also added several comments and alert/consoles to show user info/store info.

--- Functions_Assignment/Functions_Wacky/js/script.js
+++ Functions_Assignment/Functions_Wacky/js/script.js
@@ -9,19 +9,29 @@ alert("This calculation will help you determine how many companions you will hav
 var companionTL = prompt("How many close/best friends do you have? (Don't count all your friends, just your close/best friends.)");
 
 for(companionTL ; isNaN(companionTL) || companionTL === "";){ //For loop checks to see if the user entered a number and makes sure they didn't leave the field blank.
-    companionTL =  prompt("How many close/best friends do you have? (Don't count all your friends, just your close/best friends.) Time Lords don't cheat. You must enter a number and you cannot leave the field blank.");
-}
-console.log(companionTL + " - How many close/best friends, entered by the user.");
+    companionTL =  prompt("How many friends do you have? (Count all your friends, not just your close/best friends.) Time Lords don't cheat. You must enter a number and you cannot leave the field blank."); //Re-prompts the user.
+} //end of for loop
+console.log(companionTL + " - How many close/best friends, entered by the user."); //Stores number of friends the user has.
 
-var userAge = prompt("How old are you?");
+var userAge = prompt("How old are you?"); //Prompts the user for his/her age.
+for(userAge ; isNaN(userAge) || userAge === "";){
+    userAge = prompt("How old are you? Time Lords don't cheat. You must enter a number and you cannot leave the field blank."); //Re-prompts the user for his/her age.
+} //end of for loop
+console.log(userAge + " - How old the user is, entered by the user."); //Stores the user's age.
 
-console.log(userAge + " - How old the user is, entered by the user.");
+var companionAnswer = companionI(userAge, companionTL); //invokes or calls the function companionI
+function companionI(age, friends){ //Start of the function that will calculate how many companions.
+    var totalCompanion = Math.round(age/2) + Number(friends); //The variable that calculates how many companions.
+    return totalCompanion; //Sends the variable out so the information can be retrieved.
+} //end of function.
+console.log(companionAnswer + " - Number of companions the user will have as a Time Lord, calculation."); //Stores the number of companions the user will have.
 
-var companionAnswer = companionI(userAge, companionTL);
+function TLage(Age, Friends){ //Start of the function that will calculate how many years.
+    var totalTLage = Math.round(Age/2) * Math.round(Number(Friends/2)) * 13; //The variable that calculates how many years.
+    return totalTLage; //Sends the variable out so the information can be retrieved.
+} //end of function
+var TLageAnswer = TLage(userAge, companionTL); //invokes the TLage variable.
+console.log(TLageAnswer + " - How long the user will live as a Time Lord, calculation."); //Stores number of years the user will live.
 
-function companionI(age, friends){
-    var totalCompanion = Math.round(age/2) - friends;
-    return totalCompanion
-}
-
-alert("You will have " + companionAnswer + " companions as a Time Lord.");
+alert("You will have " + companionAnswer + " companions during your time as a Time Lord."); //Alerts the user of how many companions they will have during their time as a Time Lord.
+alert("Based on this information you will live for " + TLageAnswer + " years because your companions will help keep you safe."); //Alerts the user for how many years they will live as a Time Lord.
